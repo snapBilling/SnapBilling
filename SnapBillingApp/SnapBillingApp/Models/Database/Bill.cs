235498ae755bace1a02718970236fc0242e0d0da
ShivParakhTyagi <shiv@imentor.co.in>
2017-01-10 13:08:59 +0530

    POC db

---
+++ SnapBillingApp/SnapBillingApp/Models/Database/Bill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using SQLite.Net.Attributes;
+
+namespace SnapBillingApp.Models.Database
+{
+    public class Bill
+    {
+        [PrimaryKey]
+        public int BillNumber { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+    }
+}
---
+++ SnapBillingApp/SnapBillingApp/Models/SnapSqLite.cs
@@ -0,0 +1,24 @@
+﻿
+using System.IO;
+using Windows.Storage;
+using SQLite.Net;
+using SQLite.Net.Platform.WinRT;
+
+namespace SnapBillingApp.Models
+{
+    public class SnapSqLite
+    {
+        public const string DbName = "db-sqlite";
+
+        public static SQLiteConnection GetSqlConnection()
+        {
+            string path = Path.Combine(ApplicationData.
+                Current.LocalFolder.Path, DbName);
+
+            SQLiteConnection conn = new SQLiteConnection(new
+                SQLitePlatformWinRT(), path);
+
+            return conn;
+        }
+    }
+}
